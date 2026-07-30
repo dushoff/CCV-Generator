dc90900d259c1ed447eccf6badb2a534045fe1dd
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-07-30 11:58:01 -0400

    new file:   jd.mk

---
+++ jd.mk
@@ -0,0 +1,44 @@
+## This is a stealth Makefile for JD's ccv_generator fork
+
+## ln -fs jd.mk Makefile ##
+
+## This section is for Dushoff-style vim-setup and vim targeting
+## You can delete it if you don't want it
+current: target
+-include target.mk
+Ignore = target.mk
+
+vim_session:
+	bash -ic "vmt"
+
+## -include makestuff/perl.def
+
+######################################################################
+
+Ignore += Makefile ccv_generator.egg-info/
+Sources += jd.mk
+
+Sources += $(wildcard ccv_generator/*.py)
+
+######################################################################
+
+### Makestuff
+
+Ignore += makestuff
+msrepo = https://github.com/dushoff
+
+## ln -s ../makestuff . ## Do this first if you want a linked makestuff
+Makefile: makestuff/00.stamp
+makestuff/%.stamp: | makestuff
+	- $(RM) makestuff/*.stamp
+	cd makestuff && $(MAKE) pull
+	touch $@
+makestuff:
+	git clone --depth 1 $(msrepo)/makestuff
+
+-include makestuff/os.mk
+
+## -include makestuff/pipeR.mk
+
+-include makestuff/git.mk
+-include makestuff/visual.mk
